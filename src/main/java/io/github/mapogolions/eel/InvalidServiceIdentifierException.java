840dd1cdecc02fdd3f19f2e69a70936127bfba2a
Pavel Ivanov <ivanovpavelalex45@gmail.com>
2019-04-29 15:30:58 +0500

    Removed useless exception class

--- src/main/java/io/github/mapogolions/eel/InvalidServiceIdentifierException.java
+++
@@ -1,9 +0,0 @@
-package io.github.mapogolions.eel;
-
-final class InvalidServiceIdentifierException extends IllegalArgumentException {
-  private static final long serialVersionUID = -5145219757561215162L;
-
-  public InvalidServiceIdentifierException(String id) {
-    super(String.format("Identifier %s does not contain an object definition", id));
-  }
-}
